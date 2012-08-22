0a4bb154dec76be04d3094323ea2ef8d67aae174
fdammeke <fabian.dammekens@cegeka.be>
2012-08-22 12:12:17 +0200

    Added some examples

--- examples/init.pp
+++ examples/init.pp
@@ -1,1 +1,13 @@
-include puppet-jenkins
+include jenkins
+jenkins::plugin { 'ircbot':
+  version    => '2.18',
+  plugin_dir => '/data/jenkins/plugins',
+}
+jenkins::plugin { 'instant-messaging':
+  version    => '1.16',
+  plugin_dir => '/data/jenkins/plugins',
+}
+jenkins::plugin { 'active-directory':
+  version    => 'latest',
+  plugin_dir => '/data/jenkins/plugins',
+}
